21727338adb1c146770a32559721fac24012a7d7
SuhasiniSingh535 <suhasinisingh015@gmail.com>
2024-11-23 00:12:03 +0530

    Time: 0 ms (100%), Space: 10 MB (91.39%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,24 @@
+int majorityElement(int* nums, int numsSize) 
+{
+    int candidate = nums[0];
+    int count =1;
+    int i;
+    for(i=1;i<numsSize;i++)
+    {
+        if(nums[i]==candidate)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+            if(count==0)
+            {
+                candidate=nums[i];
+                count=1;
+            }
+        }
+    }
+    return candidate;
+    
+}

05c990c366a51da54dcc3a3b3e7cf48a56a4bd57
SUHASINI SINGH <suhasinisingh015@gmail.com>
2025-05-09 13:52:59 +0530

    Time: 0 ms (100%), Space: 28.2 MB (58.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,27 +5,18 @@ class Solution
         {
             int n = nums.size();
            
-            int freq=1; 
-            
-            sort(nums.begin(),nums.end());
-             if(n==1)
-                return nums[0];
-            for(int i=1;i<n;i++)
+            int freq=0;
+            int ans=0; 
+            for(int i=0;i<n;i++)
             {
-                if(nums[i]==nums[i-1])
-                {
+                if(freq==0)
+                    ans=nums[i];
+                if(ans==nums[i])
                     freq++;
-                }
                 else
-                {
-                    freq=1;
-                    
-                }
-                if(freq>n/2)
-                {
-                    return nums[i];
-                }
+                    freq--;
             }
-            return 0;
+            
+            return ans;
         }
 };
